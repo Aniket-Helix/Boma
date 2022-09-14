bb4623e804e06c3594229e69b66a706daa95494f
Aniket <aniket@velocityworks.io>
2022-09-14 19:15:23 +0530

    CRUD APIs

---
+++ pages/api/get.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { PrismaClient } from '@prisma/client'
+import requireAccess from '../../auth/require-access';
+const prisma = new PrismaClient()
+
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed' })
+    }
+    let getUser = {};
+    try {
+        const {id} = req.query;
+        const user_id: number = parseInt(id.toString())
+        const users = await requireAccess({user_id,})
+        if (users.length > 0)
+            getUser = await prisma.access.findMany({
+                where: { user_id },
+                include: {
+                    project: true,
+                },
+            });
+        res.status(200).json(getUser)
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+
+}
